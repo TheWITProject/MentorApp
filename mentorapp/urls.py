1f79779f2aa9a3f285cb20aa3352566a5c0794d1
Juliana Emmanuelli <jremmanue@gmail.com>
2020-03-08 19:39:08 -0400

    signing up a user

--- mentorapp/urls.py
+++ mentorapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from yearUpApp import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
 ]
