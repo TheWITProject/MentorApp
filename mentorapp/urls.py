576350ef41bf6017950d52301652eea9861c1dd1
Juliana Emmanuelli <jremmanue@gmail.com>
2020-03-09 19:20:40 -0400

    adding profile

--- mentorapp/urls.py
+++ mentorapp/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='yearUpApp/login.html'), name='login' ),
     path('logout/', auth_views.LogoutView.as_view(template_name='yearUpApp/logout.html'), name='logout' ),
     path('account_activation_sent/', views.account_activation_sent, name='account_activation_sent'),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.activate, name='activate'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
