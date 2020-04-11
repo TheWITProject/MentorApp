9a38b568218252bebf5f63c9197c5674cef7db0d
Your Name <you@example.com>
2020-04-11 15:56:12 -0400

    new profile chanages

--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mentorapp',
-        'USER': 'postgres',
-        'PASSWORD': 'NEW_PASSWORD',
+        'USER': 'name',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
 
     }
 }
