472c5bed9968b380fc5f0a1abea27cf65bc3faf5
Juliana Emmanuelli <jremmanue@gmail.com>
2020-04-18 12:12:20 -0400

    removig older apps

--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userProfile.apps.UserProfileConfig',
-   # 'survey.apps.SurveyConfig',
     'api.apps.ApiConfig',
     "survey",
     "bootstrapform",
