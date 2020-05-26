571ad0a81392af16cb337a11abc0232d9cd27a72
Yasiris Ortiz <yasirisortiz24@gmail.com>
2020-05-26 17:25:40 -0400

    adding jupyter notebook

--- ml/classifier/mentor_match.py
+++ ml/classifier/mentor_match.py
@@ -1,52 +1,54 @@
-import joblib # Joblib is such an pacakge that can simply turn our Python code into parallel computing mode and of course increase the computing speed.
+import joblib # Joblib is such an package that can simply turn our Python code into parallel computing mode and of course increase the computing speed.
 import pandas as pd
 
 
 class MentorMatchClassifier: # constructor to load preprocesign objects from jupiter notebook
     def __init__(self):
-        path_to_responses_file = "../../research/" # load our data from jupyter noteboook
-        self.encoders = joblib.load(path_to_responses_file + "encoders.joblib") # reconstruct a python object from a file with the use of joblib
-        self.model = joblib.load(path_to_responses_file + "random_forest.joblib") 
-# 
+        path_to_responses_file = "../../matching_algo_notebook/" # load our data from jupyter noteboook
+        self.FormatFile = joblib.load(path_to_responses_file + "FormatFile.joblib")
+        self.map_Mwhy = joblib.load(path_to_responses_file + "map_Mwhy.joblib") # reconstruct a python object from a file with the use of joblib
+        self.map_professions = joblib.load(path_to_responses_file + "map_professions.joblib") 
+        self.join_df = joblib.load(path_to_responses_file + "join_df.joblib") 
+        self.user_id = joblib.load(path_to_responses_file + "user_id.joblib") 
+        self.score_df = joblib.load(path_to_responses_file + "score_df.joblib") 
+        self.assign_id = joblib.load(path_to_responses_file + "assign_id.joblib") 
+        self.final_score = joblib.load(path_to_responses_file + "final_score.joblib")
+        self.group_equal = joblib.load(path_to_responses_file + "group_equal.joblib")
+        self.insert_score = joblib.load(path_to_responses_file + "insert_score.joblib")
+        self.top_matches_mentor = joblib.load(path_to_responses_file + "top_matches_mentor.joblib")
+        self.top_matches_student = joblib.load(path_to_responses_file + "top_matches_student.joblib")
+        self.hospital_resident = joblib.load(path_to_responses_file + "hospital_resident.joblib")
+        
+#
 
 # creating preprocessig method which takes JSON data as input 
 def preprocessing(self, input_data):
-    #add all preprocessing steps 
-    input_data = pd.DataFrame(input_data, index = 0)
-    mapping  = map_Mwhy(path_to_responses_file)
-    map_pro = map_professions()
+    # add all preprocessing steps : 
+    response_file = self.FormatFile(input_data)
+    var = self.map_Mwhy(response_file)
+    var = self.map_professions(var)
+    match_df = self.join_df(response_file, var)
+    match_df = self.user_id(match_df) #c is match_df 
+    match_sc = self.score_df(match_df)
+    match_sc = self.assign_id(match_df, match_sc) #match_sc
     
-    #apply pre-processing (cleanup and scoring)
-    for column in [
-        'Timestamp',
-        'mOs',
-        'Mindustry',
-        'Bregion',
-        'mWhy',
-        'Mwhy',
-        'gender', 
-        'yearsExp',
-        'CompSize',
-        'Profession',
-        'track',
-        'ethnicity',
-        'Growth',
-        'Groups'
-    ]:
-        #cleaning data
-        categorical_convert = self.encoders[column] #
-        input_data[column] = categorical_convert.transform(input_data[column])
 
-    return input_data
+    return match_sc, match_df
 
 #create a predict the method that intakes processed, score data and returns 1-1 match
 # Predict method : this method calls ML for computing predictions on prepared data,
 def predict(self, input_data):
-    return self.model.predict_proba(input_data) # Predict_proba will give the only probability of 1 
+    match_sc, match_df = preprocessing(input_data)
+    match_sc = self.insert_score(match_sc, match_df) # new mathc_sc
+    mentor_pref_dict = self.top_matches_mentor(match_sc) #returns mentor_pref_df
+    student_pref_dict = self.top_matches_student(match_sc) #return student_pref
+    matches = self.hospital_resident(mentor_pref_dict,student_pref_dict)
 
+    return matches # Predict_proba will give the only probability of 1 
 
 #create a post processing method that takes in match results and returns it in a serialized format
 # FIXED
+
 def postprocessing(self, input_data):
 
     label = "GoodMatch" # predict whether or not we have a match 
