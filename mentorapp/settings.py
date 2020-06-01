8cef6ac8f82831963829ce52fe6aa61c068f1fdf
Shylee Ezroni <ezronis@MacBook-Pro.fios-router.home>
2020-06-01 17:04:05 -0400

    endpoint with json results is working

--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -91,6 +91,8 @@ WSGI_APPLICATION = 'mentorapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 if 'RDS_DB_NAME' in os.environ:
+    EMAIL_HOST_USER = os.environ['email_user']
+    EMAIL_HOST_PASSWORD = os.environ['email_pass']
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -165,8 +167,8 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ['email_user']
-EMAIL_HOST_PASSWORD = os.environ['email_pass']
+EMAIL_HOST_USER = 'random@gmail.com'
+EMAIL_HOST_PASSWORD = 'random'
 EMAIL_PORT = 587
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- survey/views/survey_result.py
+++ survey/views/survey_result.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -8,7 +9,6 @@ from django.shortcuts import get_object_or_404, redirect
 
 from survey.exporter.csv.survey2csv import Survey2Csv
 from survey.models import Survey
-
 
 def serve_unprotected_result_csv(survey):
     """ Return the csv corresponding to a survey. """
@@ -21,12 +21,26 @@ def serve_unprotected_result_csv(survey):
     response["Content-Disposition"] = content_disposition
     return response
 
+def serve_unprotected_result_json(survey):
+    """ Return the csv corresponding to a survey. """
+    survey_to_csv = Survey2Csv(survey)
+    if survey_to_csv.need_update():
+        survey_to_csv.generate_file()
+        df = pd.read_csv(survey_to_csv.filename)
+        df.to_json(survey_to_csv.filename)
+    with open(survey_to_csv.filename, "r") as json_file:
+        response = HttpResponse(json_file.read(), content_type="application/json")
+    return response
 
 @login_required
 def serve_protected_result(request, survey):
     """ Return the csv only if the user is logged. """
     return serve_unprotected_result_csv(survey)
 
+@login_required
+def serve_protected_result_json(request, survey):
+    """ Return the csv only if the user is logged. """
+    return serve_unprotected_result_json(survey)
 
 def serve_result_csv(request, primary_key):
     """ ... only if the survey does not require login or the user is logged.
@@ -39,3 +53,18 @@ def serve_result_csv(request, primary_key):
     if survey.need_logged_user:
         return serve_protected_result(request, survey)
     return serve_unprotected_result_csv(survey)
+
+def serve_result_json(request, primary_key):
+    """ ... only if the survey does not require login or the user is logged.
+
+    :param int primary_key: The primary key of the survey. """
+    survey = get_object_or_404(Survey, pk=primary_key)
+    if not survey.is_published:
+        messages.error(request, "This survey has not been published")
+        return redirect("%s?next=%s" % (settings.LOGIN_URL, request.path))
+    if survey.need_logged_user:
+        return serve_protected_result_json(request, survey)
+    return serve_unprotected_result_json(survey)
+
+
+
