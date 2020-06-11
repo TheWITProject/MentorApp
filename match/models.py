e0bfe479cb06716d59350092a33f656af1c3e3aa
amaliatorres2 <amaliat209@gmail.com>
2020-06-11 18:23:47 -0400

    download csv function :)

--- match/models.py
+++ match/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from userProfile.models import Profile
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Response_json(models.Model):
@@ -7,3 +9,9 @@ class Response_json(models.Model):
 class Matches(models.Model):
     user_id = models.CharField(max_length=1000)
     match_id = models.CharField(max_length=1000)
+
+
+    puser = models.ForeignKey(Profile, on_delete=models.SET_NULL, verbose_name=_("User"), null=True, blank=True)
+
+    def __str__(self):
+        return self.user_id
